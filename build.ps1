a0ef2747efba3d5333fb1884a38973cd53de679e
Lostpold88 <Lostpold88@users.noreply.github.com>
2026-07-28 23:38:27 +0200

    docs/refactor: Remove obsolete plugin.yml in favor of native paper-plugin.yml

--- build.ps1
+++ build.ps1
@@ -27,7 +27,7 @@ New-Item -ItemType Directory -Path $binDir | Out-Null
 Write-Host "1. Compiling Java source files..."
 javac -cp $cp -d $binDir $sources
 
-Write-Host "2. Copying resources (paper-plugin.yml, plugin.yml, map.zip)..."
+Write-Host "2. Copying resources (paper-plugin.yml, map.zip)..."
 if (Test-Path $resDir) {
     Copy-Item -Path "$resDir\*" -Destination $binDir -Recurse -Force
 }
