f4d7302d35353ac3400ccb06a35d9b93c3aee5b2
Lostpold88 <Lostpold88@users.noreply.github.com>
2026-07-29 11:46:04 +0200

    revert: Revert import refactoring commit as requested by user

--- OneShotOneKill_26.1.2/src/main/java/de/oneshotonekill/manager/KillstreakManager.java
+++ OneShotOneKill_26.1.2/src/main/java/de/oneshotonekill/manager/KillstreakManager.java
@@ -4,15 +4,7 @@ import de.oneshotonekill.OneShotOneKill;
 import io.papermc.paper.threadedregions.scheduler.ScheduledTask;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
-import org.bukkit.Particle;
-import org.bukkit.Sound;
-import org.bukkit.SoundCategory;
-import org.bukkit.World;
-import org.bukkit.entity.AbstractArrow;
+import org.bukkit.*;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
@@ -21,14 +13,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.util.Vector;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Random;
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 import java.util.function.Consumer;
 
 public class KillstreakManager {
